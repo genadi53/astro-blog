506e7b789a762d5741d2f620307eebf6e6dc407c
genadi53 <kotka5353@gmail.com>
2024-10-20 16:39:45 +0300

    update header and footer

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,19 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
+
+const blogSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  // Transform string to Date object
+  pubDate: z.coerce.date(),
+  updatedDate: z.coerce.date().optional(),
+  heroImage: z.string().optional(),
+});
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  schema: blogSchema,
 });
+
+export type BlogPost = z.infer<typeof blogSchema>;
 
 export const collections = { blog };
