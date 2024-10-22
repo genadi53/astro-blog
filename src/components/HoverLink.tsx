e32cb6ee428e435c367df35eb1caae9d7690eb48
genadi53 <kotka5353@gmail.com>
2024-10-22 16:17:04 +0300

    update footer and header links

---
+++ src/components/HoverLink.tsx
@@ -0,0 +1,67 @@
+import { cn } from "@/lib/cn";
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "./ui/hover-card";
+import { CircleUser, Mail } from "lucide-react";
+import { GitHubLogoIcon, LinkedInLogoIcon } from "@radix-ui/react-icons";
+import { GITHUB_PROFILE, LINKEDIN_PROFILE, PORTFOLIO_ADDRESS } from "@/consts";
+
+export function HoverLinkPortfolio() {
+  return (
+    <HoverCard>
+      <HoverCardTrigger href={PORTFOLIO_ADDRESS}>
+        <span className="sr-only">My Website</span>
+        <CircleUser className="size-6" />
+      </HoverCardTrigger>
+      <HoverCardContent className={cn("w-fit h-fit p-1.5 text-base")}>
+        Checkout my website
+      </HoverCardContent>
+    </HoverCard>
+  );
+}
+
+export function HoverLinkGitHub() {
+  return (
+    <HoverCard>
+      <HoverCardTrigger href={GITHUB_PROFILE}>
+        <>
+          <span className="sr-only">My GitHub profile</span>
+          <GitHubLogoIcon className="size-6" aria-hidden />
+        </>
+      </HoverCardTrigger>
+      <HoverCardContent className={cn("w-fit h-fit p-1.5 text-base")}>
+        My Github Profile
+      </HoverCardContent>
+    </HoverCard>
+  );
+}
+
+export function HoverLinkMail() {
+  return (
+    <HoverCard>
+      <HoverCardTrigger href="mailto:genadi.tsolov@gmail.com">
+        <>
+          <span className="sr-only">Send me an email</span>
+          <Mail className="size-6" />
+        </>
+      </HoverCardTrigger>
+      <HoverCardContent className={cn("w-fit h-fit p-1.5 text-base")}>
+        Send me an email
+      </HoverCardContent>
+    </HoverCard>
+  );
+}
+
+export function HoverLinkLinkedIn() {
+  return (
+    <HoverCard>
+      <HoverCardTrigger href={LINKEDIN_PROFILE}>
+        <>
+          <span className="sr-only">Linkedin Profile</span>
+          <LinkedInLogoIcon className="size-6" aria-hidden />
+        </>
+      </HoverCardTrigger>
+      <HoverCardContent className={cn("w-fit h-fit p-1.5 text-base")}>
+        My LinkedIn Profile
+      </HoverCardContent>
+    </HoverCard>
+  );
+}
--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,7 @@
 
 export const SITE_TITLE = "Genadi Tsolov's Blog";
 export const SITE_DESCRIPTION = "Welcome to my personal blog!";
+export const GITHUB_PROFILE = "https://github.cm/genadi53";
+export const LINKEDIN_PROFILE =
+  "https://www.linkedin.com/in/genadi-ts%D0%BEl%D0%BEv/";
+export const PORTFOLIO_ADDRESS = "https://genadi-tsolov.vercel.app";
